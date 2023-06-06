d9448dccc70db22fd39a4aab85488c4b45d5da71
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-06 23:24:39 +0530

    Two Sum

--- 0001-TwoSum.java
+++ 0001-TwoSum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+         // Using nested loops
         int[] arr = new int[2];
         for(int i=0; i<nums.length; i++){
             int number = nums[i];
